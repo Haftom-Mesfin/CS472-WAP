e60bb0db91e925beffb9c88618a946560c229850
Haftom-Mesfin <sadtime1906@gmail.com>
2020-04-27 17:57:37 -0500

    lab6 final

---
+++ Lab6/js/app.js
@@ -0,0 +1,34 @@
+/**
+ * app.js
+ */
+
+window.onload = function() {
+
+    "use strict";
+
+    var bigBtn = document.getElementById("docBig");
+    // using anonmous function
+    bigBtn.onclick = function() {  
+        setInterval(docorateBig, 200);
+    };
+
+    function docorateBig() {
+        var textArea = document.getElementById("orginalText");
+        var style = window.getComputedStyle(textArea);
+        var fontSize = style.getPropertyValue('font-size');
+        var fontBigger = parseInt(fontSize) + 2; //update the font periodically 
+        document.getElementById("orginalText").style.fontSize = fontBigger.toString() + "px";
+    }
+
+
+    $(document).ready(function() {
+        $('input[type="checkbox"]').click(function() {
+            if ($(this).prop("checked") === true) {
+                document.getElementById("orginalText").className = "bold";
+                document.body.style.backgroundImage = "url(http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg)";
+            } else if ($(this).prop("checked") === false) {
+                document.getElementById("orginalText").className = "normalText";
+            }
+        });
+    });
+};
